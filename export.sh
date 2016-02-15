62d78dbc08a0c1ad943cb7d52479a6e32eee088a
eliranmal <eliranmal@gmail.com>
2016-02-15 23:40:54 +0200

    verbose script

--- export.sh
+++ export.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 target=$1
+target_app_path=../$target/app/
+target_export_zip=../export/$target.zip
 
-pushd ../$target/app/ \
-    && zip -v ../export/$target.zip -r * \
+echo "target: $target"
+echo "target_app_path: $target_app_path"
+echo "target_export_zip: $target_export_zip"
+
+pushd $target_app_path \
+    && zip -v $target_export_zip -r * \
     && popd
